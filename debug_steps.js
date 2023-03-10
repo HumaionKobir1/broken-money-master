776b0a67211932bd620f8c63b9fe13ae5d242ee2
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-03-10 20:43:59 +0600

    Added => some debugging technique

---
+++ debug_steps.js
@@ -0,0 +1,14 @@
+// Debug steps
+/* 
+1. Go to the live website. And try to reproduce the error.
+2. Check the console of the live website to see any error/exception
+3. Read exception thoroughly. In detail
+4. If the error is new to you: copy the text of the error and search online.
+5.if a line number is showing, click on that line to see the code
+6. Add breakpoint. re run the code (refresh the website). and see wether you are getting the expected values.
+    6.1: if you are not getting the expected values, check which values you are getting If needed  break point in the previous line
+    6.2: if needed, step into (F11) to a function
+
+
+InnerText --> value
+*/
